fbe7d2539bd60742264421a3d05473322351a787
Yonghun Park <yw9142@gmail.com>
2022-04-27 00:39:16 +0900

    refactor: utterances margin 수정

--- src/theme/DocItem.tsx
+++ src/theme/DocItem.tsx
@@ -3,6 +3,7 @@ import DocItem from '@theme-original/DocItem';
 import Comment from "../components/Comment";
 
 export default function DocItemWrapper(props) {
+
     return (
         <>
             <DocItem {...props} />
